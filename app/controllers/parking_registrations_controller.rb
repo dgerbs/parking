0acd2ce396e7268f934460f846591879cdfc54b6
Doug Gerber <dojoh@outlook.com>
2013-09-22 20:10:57 -0400

    Add Email Store

--- app/controllers/parking_registrations_controller.rb
+++ app/controllers/parking_registrations_controller.rb
@@ -1,29 +1,30 @@
 class ParkingRegistrationsController < ApplicationController
-	def new
-		@parking_registration = ParkingRegistration.new
-	end
+  def new
+    @last_registration = ParkingRegistration.find_by_id(session[:last_registration_id])
+    @parking_registration = ParkingRegistration.new
 
-	def create
-		@parking_registration = ParkingRegistration.new(reg_params)
-		if @parking_registration.park
-			flash[:notice] = 'You registered successfully.'
-			redirect_to "/parking_registrations/#{@parking_registration.id}"
-		else
-			render :new
-		end
-	end
+    @parking_registration.email = @last_registration.try(:email)
 
-	def show
-		@parking_registration = ParkingRegistration.find(params[:id])
-	end
+  end
 
-	protected
+  def create
+    @parking_registration = ParkingRegistration.new(reg_params)
+    if @parking_registration.park
+      session[:last_registration_id] = @parking_registration.id
+      flash[:notice] = 'You registered successfully'
+      redirect_to parking_registration_path(@parking_registration)
+    else
+      render :new
+    end
+  end
 
-	def reg_params
-		params.require(:parking_registration).permit(
-			:first_name, 
-			:last_name, 
-			:email, 
-			:spot_number)
-	end
+  protected
+
+  def reg_params
+    params.require(:parking_registration).permit(
+      :first_name,
+      :last_name,
+      :email,
+      :spot_number)
+  end
 end
